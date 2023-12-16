99020a3409536d57d150e834ccef4ef8cd69803c
Akriti Sengar <85075827+Tashuuuu@users.noreply.github.com>
2023-12-16 23:07:47 +0530

    Update 003-Stone-Paper-Scissor.py

--- Beginner/003-Stone-Paper-Scissor.py
+++ Beginner/003-Stone-Paper-Scissor.py
@@ -1,21 +1,21 @@
 import random
 choices = ['stone', 'paper', 'scissor']
 while True:
-    computerC = random.choice(choices)
-    userC = input('Enter your choice (stone/paper/scissor): ').lower()
-    if userC =='exit':
+    computer_choice = random.choice(choices)
+    user_choice = input('Enter your choice (stone/paper/scissor): ').lower()
+    if user_choice =='exit':
         break
-    while userC not in choices:
+    while user_choice not in choices:
         print('Please enter a valid choice!!')
-        userC = input('enter your choice (stone/paper/scissor): ').lower()
-    print('user:', userC)
-    print('computer:', computerC)
+        user_choice = input('enter your choice (stone/paper/scissor): ').lower()
+    print('user:', user_choice)
+    print('computer:', computer_choice)
 
-    if userC == computerC:
+    if user_choice == computer_choice:
         print('Its a tie')
-    elif ((userC == 'stone' and computerC == 'scissor')
-            or (userC == 'paper' and computerC == 'stone')
-            or (userC == 'scissor' and computerC == 'paper')):
+    elif ((user_choice == 'stone' and computer_choice == 'scissor')
+            or (user_choice == 'paper' and computer_choice == 'stone')
+            or (user_choice == 'scissor' and computer_choice == 'paper')):
         print('You win🤩!')
     else:
         print('Computer win🤖')
