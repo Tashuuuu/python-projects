c6b2e3518b596a5e22c221580a8e6e99ba8572e6
Akriti Sengar <85075827+Tashuuuu@users.noreply.github.com>
2023-12-17 19:35:13 +0530

    Update projects.py

--- Random projects/projects.py
+++ Random projects/projects.py
@@ -344,6 +344,37 @@ while True:
     break
   else:
     print("Please choose only from the mentioned options.")
+# Another way to solve it is:
+def add(n1, n2):
+    return n1 + n2
+def subtract(n1, n2):
+    if n1 > n2:
+        return n1 - n2
+    return n2 - n1
+def divide(n1, n2):
+    if n1 > n2:
+        return n1 / n2
+    return n2 / n1
+def multiply(n1, n2):
+    return n1 * n2
+
+operation = {'+': add, '-': subtract, '/': divide, '*': multiply}
+
+continue_choice = 'yes'
+
+while continue_choice == 'yes':
+    num1 = int(input("\nEnter the first number: "))
+    num2 = int(input("Enter the second number: "))
+
+    for symbol in operation:
+        print(symbol, end="  ")
+    operation_symbol = input("\n Pick an operation from the line above: ")
+
+    cal = operation[operation_symbol]
+    result = cal(num1, num2)
+
+    print(f"{num1} {operation_symbol} {num2} = {result}")
+    continue_choice = input('\n Do you want to perform another operation? [yes/no] \n')
 
 
 
