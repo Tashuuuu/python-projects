24c70e20ec31097d5af0af4ef37fb2551a2d63e9
Akriti Sengar <85075827+Tashuuuu@users.noreply.github.com>
2024-01-01 18:15:04 +0530

    Update and rename 016 to 016-Word-Counter.py

---
+++ Beginner/016-Word-Counter.py
@@ -0,0 +1,4 @@
+text = input("Enter your text: ")
+word = text.split()
+print(f"Your given text is \'{text}\'") # It's optional. If you want to show them their sentence.
+print(f"The number of words in your given text is {len(word)}")
