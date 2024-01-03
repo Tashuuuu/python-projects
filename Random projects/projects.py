5f36a3bf1d7e3cc7bbbadff85f15e3c8180d857d
Akriti Sengar <85075827+Tashuuuu@users.noreply.github.com>
2024-01-03 23:49:58 +0530

    Update projects.py

--- Random projects/projects.py
+++ Random projects/projects.py
@@ -33,23 +33,6 @@ while continue_choice == 'yes':
 
 
 
-# 14. Find prime numbers:
-num = int(input("Enter the number you want to check: "))
-div = 2
-if (num == 2 or num == 1):
-  print(num, "is a prime number.")
-else:
-  while num > div:
-    if num % div == 0:
-      print(num, "is not prime number. Because it is divisible by", div)
-      break
-    else:
-      div = div + 1
-  if num == div:
-      print(num, "is a prime number.")
-
-
-
 # 15. Print all prime numbers till n:
 n = int(input("Enter the value of n: "))
 num = 3
