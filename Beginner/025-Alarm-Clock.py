745259eddf91108bfa6cc019fd89b84f0314ff34
Akriti Sengar <85075827+Tashuuuu@users.noreply.github.com>
2024-01-09 20:16:11 +0530

    Create 025-Alarm-Clock.py

---
+++ Beginner/025-Alarm-Clock.py
@@ -0,0 +1,35 @@
+# Day 4: Alarm Clock 
+import time
+import playsound
+from playsound import playsound
+current_time = time.strftime("%H %M")
+alarm_time = 
+if current_time == "23 42":
+  playsound("Sound.mp3")
+
+# OR
+
+from datetime import datetime
+from playsound import playsound
+alarm_time = input("Enter the time of alarm to be set:HH:MM:SS\n")
+alarm_hour = alarm_time[0:2]
+alarm_minute = alarm_time[3:5]
+alarm_seconds = alarm_time[6:8]
+alarm_period = alarm_time[9:11].upper()
+print("Setting up alarm..")
+while True:
+    now = datetime.now()
+    current_hour = now.strftime("%I")
+    current_minute = now.strftime("%M")
+    current_seconds = now.strftime("%S")
+    current_period = now.strftime("%p")
+    if(alarm_period == current_period):
+        if(alarm_hour == current_hour):
+            if(alarm_minute == current_minute):
+                if(alarm_seconds == current_seconds):
+                    print("Wake Up!")
+                    playsound('Sound.mp3')
+                    break
+
+
+
